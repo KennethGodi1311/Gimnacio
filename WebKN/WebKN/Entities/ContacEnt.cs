5e263ba2a7299f458803c81e504ed043ed1eb28f
KennethGodi1311 <106417438+KennethGodi1311@users.noreply.github.com>
2023-11-19 00:52:28 -0600

    cambios de vistas
    cambios de modelos y vistas

--- WebKN/WebKN/Entities/ContacEnt.cs
+++ WebKN/WebKN/Entities/ContacEnt.cs
@@ -7,8 +7,8 @@ namespace WebKN.Entities
 {
     public class ContacEnt
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
+            public int Id { get; set; }
+            public string Nombre { get; set; }
             public string Correo { get; set; }
             public string Asunto { get; set; }
             public string Mensaje { get; set; }
